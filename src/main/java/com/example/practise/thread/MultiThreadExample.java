0dc8349ff6bd58022040a33b0baf992c4eff81f5
Sean <asd56494783@gmailgit config --global user.email asd56494783@gmail.com>
2023-07-17 02:42:47 +0800

    simpleThreadExercise

--- src/main/java/com/example/practise/thread/MultiThreadExample.java
+++ src/main/java/com/example/practise/thread/MultiThreadExample.java
@@ -40,5 +40,70 @@ public class MultiThreadExample {
             }
         }
     }
+
+
+
+
+//    public static void main(String[] args) {
+//        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+//        int numThreads = 4;
+//
+//        int chunkSize = numbers.length / numThreads;
+//
+//        SumCalculator[] calculators = new SumCalculator[numThreads];
+//        Thread[] threads = new Thread[numThreads];
+//
+//        // 創建並啟動計算執行緒
+//        for (int i = 0; i < numThreads; i++) {
+//            int startIndex = i * chunkSize;
+//            int endIndex = (i == numThreads - 1) ? numbers.length : (i + 1) * chunkSize;
+//
+//            calculators[i] = new SumCalculator(numbers, startIndex, endIndex);
+//            threads[i] = new Thread(calculators[i]);
+//            threads[i].start();
+//        }
+//
+//        // 等待所有計算執行緒完成
+//        try {
+//            for (Thread thread : threads) {
+//                thread.join();
+//            }
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//
+//        // 計算總和
+//        int totalSum = 0;
+//        for (SumCalculator calculator : calculators) {
+//            totalSum += calculator.getSum();
+//        }
+//
+//        System.out.println("Total sum: " + totalSum);
+//    }
+//
+//    static class SumCalculator implements Runnable {
+//        private int[] numbers;
+//        private int startIndex;
+//        private int endIndex;
+//        private int sum;
+//
+//        public SumCalculator(int[] numbers, int startIndex, int endIndex) {
+//            this.numbers = numbers;
+//            this.startIndex = startIndex;
+//            this.endIndex = endIndex;
+//        }
+//
+//        @Override
+//        public void run() {
+//            sum = 0;
+//            for (int i = startIndex; i < endIndex; i++) {
+//                sum += numbers[i];
+//            }
+//        }
+//
+//        public int getSum() {
+//            return sum;
+//        }
+//    }
 }
 
